0c25da3085393ab9f62e0e52e17fa1480fdee2af
步浪 <1107842278@qq.com>
2018-05-15 17:00:38 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -3,8 +3,8 @@ var conf = {
     "secret":"authorIsBuLang",
     "code_exp":180,
     "aliDaYu":{
-        app_key: '24816279',
-        app_secret: '3180c8815d1d949d9d45b6c50d4d83ab'
+        app_key: '',
+        app_secret: ''
     }
 };
 
